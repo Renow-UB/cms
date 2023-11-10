495f3ce8c791bb68a519497424492349799ee6c7
Trygve <trygvemf@gmail.com>
2023-11-10 10:29:43 +0100

    v1.0

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div className="w-full flex justify-center items-center p-10">
+			<h1 className="font-bold">CMS Test</h1>
+		</div>
+	);
+};
+
+export default Header;
